c6ae415e454ca68a0897417ef8cb004f798eb438
AndyS120 <asalin12@gmail.com>
2023-09-08 22:19:44 -0400

    Added code for API interaction

--- src/main/java/com/csc340f23/apiprototype/ApiPrototypeApplication.java
+++ src/main/java/com/csc340f23/apiprototype/ApiPrototypeApplication.java
@@ -1,13 +1,40 @@
 package com.csc340f23.apiprototype;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApiPrototypeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiPrototypeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPrototypeApplication.class, args);
+        getData();
+    }
+
+    public static void getData() {
+        try {
+            String apiString = "https://api.animality.xyz/fact/dog";
+
+            RestTemplate restTemplate = new RestTemplate();
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String jsonString = restTemplate.getForObject(apiString, String.class);
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+            String animal = jsonNode.findValue("animal").asText();
+            String fact = jsonNode.findValue("fact").asText();
+
+            System.out.printf("\nRandom %s fact:\n", animal);
+            System.out.println(fact);
+            System.out.println("\nRefresh for more facts!");
+        }
+        catch (JsonProcessingException processingException) {
+            System.out.println("Error: Could not process JSON");
+        }
+    }
 
 }
