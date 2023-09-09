501310e0c8509b20cf794e27bee7a3ba2537587d
AndyS120 <asalin12@gmail.com>
2023-09-08 21:15:14 -0400

    Added original SpringBoot files

---
+++ src/main/java/com/csc340f23/apiprototype/ApiPrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.csc340f23.apiprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPrototypeApplication.class, args);
+	}
+
+}
